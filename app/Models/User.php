b39d7367996de868958c33f1d72e6e89c1d49c64
Kadidia <kamanta7605@gmail.com>
2026-06-20 16:12:40 +0100

    Ajout dashboard employe et migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,20 +3,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-<<<<<<< HEAD
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable {
-    use Notifiable;
-=======
-use  App\Models\pointages;
-use App\Models\demandeConges;
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
->>>>>>> 5b37850a57da6b3fe55ae84b8c85831ff2dfe4f9
 
     protected $fillable = ['name', 'email', 'password', 'role', 'department_id', 'is_active'];
     protected $hidden   = ['password', 'remember_token'];
@@ -32,15 +23,4 @@ class User extends Authenticatable
     public function todayAttendance(): ?Attendance {
         return $this->attendances()->whereDate('date', today())->first();
     }
-<<<<<<< HEAD
 }
-=======
-    // app/Models/User.php
-public function pointages() {
-    return $this->hasMany(Pointages::class);
-}
-public function demandesConges() {
-    return $this->hasMany(DemandeConges::class);
-}
-}
->>>>>>> 5b37850a57da6b3fe55ae84b8c85831ff2dfe4f9
